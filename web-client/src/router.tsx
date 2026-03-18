9a97cc4953e512e7b53368778d4b230bdfb7d210
withakay <jack@fader.co.uk>
2026-03-18 09:40:18 +0000

    feat(web-client): add routing foundation and initialize package dependencies

--- web-client/src/router.tsx
+++ web-client/src/router.tsx
@@ -1,8 +1,17 @@
 import { createRootRoute, createRoute, createRouter, Outlet } from '@tanstack/react-router'
 
+import { AppShell } from '@/components/app-shell'
 import { HomePage } from '@/routes/home'
+import { LinksPage } from '@/routes/links'
+import { ReadmePage } from '@/routes/readme'
 
-const rootRoute = createRootRoute({ component: () => <Outlet /> })
+const rootRoute = createRootRoute({
+  component: () => (
+    <AppShell>
+      <Outlet />
+    </AppShell>
+  ),
+})
 
 const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
@@ -10,7 +19,19 @@ const indexRoute = createRoute({
   component: HomePage,
 })
 
-const routeTree = rootRoute.addChildren([indexRoute])
+const linksRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/links',
+  component: LinksPage,
+})
+
+const readmeRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/readme',
+  component: ReadmePage,
+})
+
+const routeTree = rootRoute.addChildren([indexRoute, linksRoute, readmeRoute])
 
 export const router = createRouter({ routeTree })
 
